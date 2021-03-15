bc0ac23b1bc9a521190aa3618ba7bb719664f30c
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-03-15 01:07:50 -0300

    ✨ Add main() code to instantiate a Game window

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,13 @@
+#include "Game.h"
 
-int main(int argc, char** argv) {
-  
+// used for the temporary sleep call
+#include <unistd.h>
+
+// int main(int argc, char** argv) {
+int main() {
+  Game *game = new Game("Teste", 1024, 600);
+  *game = Game::GetInstance();
+
+  // sleep to keep window open for 3s
+  sleep(3);
 }
