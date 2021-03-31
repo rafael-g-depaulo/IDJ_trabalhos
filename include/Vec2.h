a09fd24c07a273c75d75ea8dcf19c7e7dc1bc890
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-03-31 19:26:34 -0300

    ✨ Add + operator to Vec2

--- include/Vec2.h
+++ include/Vec2.h
@@ -1,13 +1,18 @@
 #ifndef vec2_h
 #define vec2_h
 
+#include <iostream>
+
 class Vec2 {
   public:
-    Vec2();
-    Vec2(float x, float y);
+    Vec2(float x = 0.0, float y = 0.0);
 
     float x;
     float y;
+
+    Vec2 operator+(const Vec2& vec);
+
+    void print(const char* name = nullptr);
 };
 
 #endif
--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -1,11 +1,24 @@
 #include "../include/Vec2.h"
 
-Vec2::Vec2() {
-  this->x = 0;
-  this->y = 0;
-}
+using namespace std;
 
-Vec2::Vec2(float x = 0, float y = 0) {
+Vec2::Vec2(float x, float y) {
   this->x = x;
   this->y = y;
+}
+
+Vec2 Vec2::operator+(const Vec2& vec) {
+  Vec2* result = new Vec2();
+  result->x = this->x + vec.x;
+  result->y = this->y + vec.y;
+  return *result;
+}
+
+
+void Vec2::print(const char* name) {
+  if (name != nullptr) {
+    cout << name << ": " << "(" << this->x << ", " << this->y << ")" << endl;
+  } else {
+    cout << "x: " << this->x << " y: " << this->y << endl;
+  }
 }
