4cd42a49344c5f2328abfd4b0c955c5f0b0a1289
Rafael G. de Paulo <rafael.g.depaulo@gmail.com>
2021-04-03 23:54:32 -0300

    ✨ Add Rect::contains and Rect::print

--- include/Rect.h
+++ include/Rect.h
@@ -9,6 +9,12 @@ class Rect {
     Rect(Vec2 pos, float w, float h);
     Rect(float x, float y, float w, float h);
 
+    void print(const char* name = nullptr);
+    
+    bool contains(int x, int y);
+    bool contains(float x, float y);
+    bool contains(Vec2 point);
+
     float x;
     float y;
     float w;
--- src/Rect.cpp
+++ src/Rect.cpp
@@ -1,5 +1,9 @@
 #include "../include/Rect.h"
 
+#include <iostream>
+
+using namespace std;
+
 Rect::Rect() {
   this->x = 0;
   this->y = 0;
@@ -19,4 +23,20 @@ Rect::Rect(float x = 0, float y = 0, float w = 0, float h = 0) {
   this->y = y;
   this->h = h;
   this->w = w;
+}
+
+void Rect::print(const char* name) {
+  if (name != nullptr) {
+    cout << name << ": ";
+  }
+  cout << "top-right" << "(" << this->x << ", " << this->y << ")" <<
+    "; bottom-left(" << this->x + this->w << ", " << this->y + this->h << ")" << endl;
+}
+
+bool Rect::contains(int x, int y) { return this->contains((float) x, (float) y); }
+bool Rect::contains(float x, float y) { return this->contains(Vec2(x, y)); }
+bool Rect::contains(Vec2 point) {
+  bool withinY = (point.y >= this->y) && (point.y <= this->y + this->h);
+  bool withinX = (point.x >= this->x) && (point.x <= this->x + this->w);
+  return withinX && withinY;
 }
